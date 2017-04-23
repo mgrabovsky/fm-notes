d80a7df9726e8ca0dfaaf1703eacc3ea83ac4a6d
Matěj Grabovský <mgrabovsky@yahoo.com>
2017-04-23 22:43:34 +0200

    coqeth: Minor cleanup

--- sketches/coqeth/EthTypes.v
+++ sketches/coqeth/EthTypes.v
@@ -4,38 +4,13 @@ Unset Strict Implicit.
 Require Fin Vector.
 
 (* Basic data types *)
-Definition bit  := bool.
-Definition byte := Vector.t bit 8.
-Definition byte_array := list byte.
-Definition word    := Vector.t bit 256.
-Definition address := Vector.t bit 160.
-Definition hash256 := Vector.t bit 256.
-Definition hash512 := Vector.t bit 512.
-
-(* Decidable equality for these types *)
-Lemma bit_eq_dec : forall x y : bit, {x = y} + {x <> y}.
-Proof. apply Bool.bool_dec. Qed.
-
-Definition bit_beq := Bool.eqb.
-
-Lemma byte_eq_dec : forall x y : byte, {x = y} + {x <> y}.
-Proof. apply (Vector.eq_dec _ bit_beq Bool.eqb_true_iff). Qed.
-
-Definition byte_beq : byte -> byte -> bool := Vector.eqb _ bit_beq.
-
-Lemma byte_array_eq_dec : forall x y : byte_array, {x = y} + {x <> y}.
-Proof. decide equality; apply byte_eq_dec. Qed.
-
-Definition byte_array_beq x y :=
-  if byte_array_eq_dec x y then true else false.
-
-Lemma address_eq_dec : forall x y : address, {x = y} + {x <> y}.
-Proof. apply (Vector.eq_dec _ bit_beq Bool.eqb_true_iff). Qed.
-
-Lemma hash256_eq_dec : forall x y : hash256, {x = y} + {x <> y}.
-Proof. apply (Vector.eq_dec _ bit_beq Bool.eqb_true_iff). Qed.
-Lemma hash512_eq_dec : forall x y : hash512, {x = y} + {x <> y}.
-Proof. apply (Vector.eq_dec _ bit_beq Bool.eqb_true_iff). Qed.
+Definition bit     := bool.
+Definition byte    := Vector.t bit 8.
+Definition byteseq := list byte.
+Definition word    := Vector.t byte 32.
+Definition address := Vector.t byte 20.
+Definition hash256 := Vector.t byte 32.
+Definition hash512 := Vector.t byte 64.
 
 (* Abstract specification of the Keccak (SHA-3) hash functions *)
 Variable KEC256 : list byte -> hash256.
