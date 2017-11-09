2c15544dda9fc917129117e1a0f7559c76f1e9b5
Matěj Grabovský <mgrabovsky@yahoo.com>
2017-11-09 14:42:01 +0100

    Coq: XOR swap proof sketch

---
+++ sketches/xorswap.v
@@ -0,0 +1,69 @@
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Import Bvector.
+Require Import Setoid.
+
+Definition byte := Bvector 8.
+Definition xor  := BVxor.
+
+Lemma xor_commut : forall n x y, @xor n x y = xor y x.
+Proof.
+  intros.
+  unfold xor, BVxor.
+  apply (Vector.rect2 (fun m v1 v2 => @Vector.map2 _ _ _ xorb m v1 v2 = Vector.map2 xorb v2 v1)).
+  - reflexivity.
+  - intros; cbn.
+    now rewrite xorb_comm, H.
+Qed.
+
+Lemma xor_assoc : forall n x y z, @xor n x (xor y z) = xor (xor x y) z.
+Proof.
+  intros.
+  unfold xor, BVxor.
+Admitted.
+
+Lemma xor_nilpotent : forall n x, @xor n x x = @Bvect_false n.
+Proof.
+  intros.
+  unfold xor, BVxor, Bvect_false.
+  induction x.
+  - reflexivity.
+  - now cbn; rewrite IHx, xorb_nilpotent.
+Qed.
+
+Lemma xor_false_r : forall n x, @xor n x (@Bvect_false n) = x.
+Proof.
+  intros.
+  unfold xor, BVxor, Bvect_false.
+  induction x.
+  - reflexivity.
+  - now cbn; rewrite IHx, xorb_false_r.
+Qed.
+
+Lemma xor_false_l : forall n x, @xor n (@Bvect_false n) x = x.
+Proof.
+  intros.
+  unfold xor, BVxor, Bvect_false.
+  induction x.
+  - reflexivity.
+  - cbn; rewrite IHx.
+    now destruct h.
+Qed.
+
+Definition swap (vars : byte * byte) :=
+  let '(x, y) := vars in
+  let x := xor x y in
+  let y := xor y x in
+  let x := xor x y in
+      (x, y).
+
+Theorem swap_correct : forall x y, swap (x, y) = (y, x).
+Proof.
+  intros; unfold swap.
+  rewrite (xor_commut y (xor x y)), xor_assoc.
+  rewrite xor_nilpotent, xor_false_l.
+  rewrite <- xor_assoc, xor_nilpotent, xor_false_r.
+  reflexivity.
+Qed.
