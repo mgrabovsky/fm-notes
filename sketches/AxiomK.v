42bc3c14450da6838c5534705bd7b8723274b313
Matěj Grabovský <matej@mgrabovsky.net>
2017-12-22 20:33:47 +0100

    Mention Axiom J in Axiom K sketch

--- sketches/AxiomK.v
+++ sketches/AxiomK.v
@@ -2,12 +2,20 @@ Axiom K : forall {A} {x : A} (C : x = x -> Type),
   C eq_refl ->
   forall loop : x = x, C loop.
 
+Lemma J : forall {A} {x : A} (C : forall y, x = y -> Type),
+  C x eq_refl ->
+  forall y (p : x = y), C y p.
+Proof.
+  intros; destruct p; assumption.
+Qed.
+
 Lemma all_refls :
   forall {A} {x : A} (p : x = x), p = eq_refl.
 Proof.
   intros.
   eapply (K (fun x => x = eq_refl) _ p).
-  Unshelve. reflexivity.
+  Unshelve.
+  { reflexivity. }
 Qed.
 
 Goal forall {A} {x y : A} (p q : x = y), p = q.
