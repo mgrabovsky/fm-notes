55a12d7293b979c642b984ae80b63e7333638f2a
Matěj Grabovský <matej@mgrabovsky.net>
2019-09-08 22:24:58 +0200

    sketches: Merge two into one

--- sketches/SemigroupPuzzles.v
+++ sketches/SemigroupPuzzles.v
@@ -1,30 +1,52 @@
 (** Source: https://golem.ph.utexas.edu/category/2015/06/semigroup_puzzles.html *)
-Require Import Coq.Setoids.Setoid.
+
+Require Import Setoid.
+Require Import ssreflect.
+
+Generalizable All Variables.
 
 Section puzzles.
   Variable A : Type.
   Variable op : A -> A -> A.
   Local Infix "*" := op (at level 40, left associativity).
 
-  Hypothesis assoc : forall x y z, x * y * z = x * (y * z).
-  Hypothesis absorp : forall x y, x * y * x = x.
+  Hypothesis assoc  : `(x * y * z = x * (y * z)).
+  Hypothesis absorp : `(x * y * x = x).
 
-  Lemma puzzle1 : forall x y z, x * y * z = x * z.
-  Proof.
-    intros x y z.
-    rewrite <- (absorp x (y*z)) at 2.
-    rewrite (assoc _ x z).
-    rewrite (assoc x (y*z) (x*z)).
-    rewrite (assoc y _ _).
-    rewrite <- (assoc z x z).
-    now rewrite absorp.
-  Qed.
+  Section vanilla.
+    Lemma puzzle1 : forall x y z, x * y * z = x * z.
+    Proof.
+      intros x y z.
+      rewrite <- (absorp x (y*z)) at 2.
+      rewrite (assoc _ x z).
+      rewrite (assoc x (y*z) (x*z)).
+      rewrite (assoc y _ _).
+      rewrite <- (assoc z x z).
+      now rewrite absorp.
+    Qed.
 
-  Lemma puzzle2 : forall x, x * x = x.
-  Proof.
-    intro x.
-    rewrite <- (absorp x x) at 1.
-    rewrite (assoc x _ x).
-    now rewrite absorp.
-  Qed.
+    Lemma puzzle2 : forall x, x * x = x.
+    Proof.
+      intro x.
+      rewrite <- (absorp x x) at 1.
+      rewrite (assoc x _ x).
+      now rewrite absorp.
+    Qed.
+  End vanilla.
+
+  Section ssr.
+    Lemma puzzle1_ssr : forall x y z, x * y * z = x * z.
+    Proof.
+      move=> x y z.
+      rewrite -{2}(absorp x (y*z)) (assoc _ x z) (assoc x (y*z) (x*z)).
+      by rewrite (assoc y _ _) -(assoc z x z) absorp.
+    Qed.
+
+    Lemma puzzle2_ssr : forall x, x * x = x.
+    Proof.
+      move=> x.
+      by rewrite -{1}(absorp x x) (assoc x _ x) absorp.
+    Qed.
+  End ssr.
 End puzzles.
+
